a737e27dfce6b80d801d59364b96392b907719a1
AlexanderTemp <aaalex2025@gmail.com>
2025-04-09 22:18:53 -0400

    feat: initial project

---
+++ README.md
@@ -0,0 +1,6 @@
+```
+curl -s "https://laravel.build/example-app" | bash
+
+sail composer require inertiajs/inertia-laravel
+
+```
---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Bienvenido" />
+    <div class="flex h-screen items-center justify-center">
+        <p class="text-white">hola mundo</p>
+    </div>
+</template>
